5b8389a6dddd4790780fff2a74c5c8d367814a10
Matt Helm <mthelm85@gmail.com>
2018-08-04 16:01:20 -0500

    added ability to join specific room, emit data to specific room

--- server.js
+++ server.js
@@ -10,9 +10,7 @@ const io = require('socket.io')(server)
 
 const port = process.env.PORT || 3000
 
-require('./routes/open-routes.js')(app, io)
-
-// Server //
+// Server Config//
 
 app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({
@@ -20,19 +18,28 @@ app.use(bodyParser.urlencoded({
 }))
 app.use(bodyParser.json())
 
-// Socket //
+// Socket.io //
 
+// handle incoming connections from clients
 io.on('connection', (socket) => {
-  console.log('New user connected...')
+  console.log(`User ${socket.id} connected`)
 
-  socket.on('weight', (data) => {
-    socket.broadcast.emit('weightUpdate', data)
+  // once client connects, send them to the appropriate room
+  socket.on('room', (roomId) => {
+    socket.join(roomId)
   })
 
+  // when scale sends weight and roomId, emit the weight to the room
+  socket.on('weight', (data) => {
+    socket.to(data.roomId).emit('weightUpdate', data.weight)
+  })
+
+  // when user sends tare request, emit to the scale
   socket.on('tare', () => {
     socket.broadcast.emit('tare')
   })
 
+  // handle user disconnections
   socket.on('disconnect', () => {
     console.log('User disconnected...')
   })
