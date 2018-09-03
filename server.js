b6ba3a24d5878bb2b4e84df7c9fcfee01a28c477
Matt Helm <mthelm85@gmail.com>
2018-09-03 18:41:32 -0500

    commented out firebaseConfig const for heroku deployment

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const bodyParser = require('body-parser'),
       server = http.createServer(app),
       port = process.env.PORT || 3000,
       User = require('./config/model-user')
-      firebaseConfig = require('./config/firebase')
+      // firebaseConfig = require('./config/firebase')
       io = require('socket.io')(server)
 
 // Connect to MongoDB
